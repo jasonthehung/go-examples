1ff70bb9cca615fb0135b9accb15d3a778736eed
Jason Wang A <jason.a.wang@ericsson.com>
2025-11-07 10:49:55 +0800

    .

--- 21.Enums/main.go
+++ 21.Enums/main.go
@@ -12,7 +12,7 @@ const (
 	StateRetrying
 )
 
-var stateName = map[ServerState]string {
+var stateName = map[ServerState]string{
 	StateIdle: "idle",
 	StateConnected: "connected",
 	StateError: "error",
@@ -25,22 +25,20 @@ func (ss ServerState) String() string {
 
 func main() {
 	ns := transition(StateIdle)
-	fmt.Println(ns)
-
-	ns2 := transition(ns)
-	fmt.Println(ns2)
-
+    fmt.Println(ns)
+    ns2 := transition(ns)
+    fmt.Println(ns2)
 }
 
 func transition(s ServerState) ServerState {
-	switch s {
-	case StateIdle:
-		return StateConnected
-	case StateConnected, StateRetrying:
-		return StateIdle
-	case StateError:
-		return StateError
-	default:
-		panic(fmt.Errorf("unknown state: %s", s))
-	}
+    switch s {
+    case StateIdle:
+        return StateConnected
+    case StateConnected, StateRetrying:
+        return StateIdle
+    case StateError:
+        return StateError
+    default:
+        panic(fmt.Errorf("unknown state: %s", s))
+    }
 }
